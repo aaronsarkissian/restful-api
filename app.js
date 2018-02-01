b129aa1411a065154e1d506e57735795b28fe343
aaronsarkissian <aaronsarkissian@gmail.com>
2018-02-02 01:16:58 +0400

    #9

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/user');
 
 mongoose.connect(`mongodb://aaronsarkissian:${process.env.MANGO_ATLAS_PW}@cluster0-shard-00-00-jsj4m.mongodb.net:27017,cluster0-shard-00-01-jsj4m.mongodb.net:27017,cluster0-shard-00-02-jsj4m.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin`, {
 	useMongoClient: true
@@ -30,6 +31,7 @@ app.use((req, res, next) => {
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use('/user', userRoutes);
 
 app.use((req, res, next) => {
 	const error = new Error('Not found');
